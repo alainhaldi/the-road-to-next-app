0ec961e4c95aa418fc4eae9f6ce33eb91fd9b072
alain <alain.haldi@pm.me>
2026-01-16 11:54:11 +0100

    Simulating data fetching of tickets

---
+++ src/features/ticket/queries/get-tickets.ts
@@ -0,0 +1,11 @@
+import { initialTickets } from "@/data";
+import { Ticket } from "../types";
+
+export const getTickets = async (): Promise<Ticket[]> => {
+  // Simulate 2sec delay
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  return new Promise((resolve) => {
+    resolve(initialTickets);
+  });
+};
