bebfc337c1f60b5819e4d2154fb57edda032119d
alain <alain.haldi@pm.me>
2026-01-17 10:46:06 +0100

    Adding error.tsx file to TicketsRoute

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -5,6 +5,8 @@ export const getTickets = async (): Promise<Ticket[]> => {
   // Simulate 2sec delay
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
+  // throw new Error("failed to fetch tickets :(");
+
   return new Promise((resolve) => {
     resolve(initialTickets);
   });
