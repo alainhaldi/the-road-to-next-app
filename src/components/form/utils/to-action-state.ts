c539fd529b789a944e4957db1733be8ecd68dd2c
alain <alain.haldi@pm.me>
2026-01-23 12:00:40 +0100

    Show Error Message below the Input fields

--- src/components/form/utils/to-action-state.ts
+++ src/components/form/utils/to-action-state.ts
@@ -1,6 +1,18 @@
 import * as z from "zod/v4";
 
-export type ActionState = { message: string; payload?: FormData };
+export type ActionState = {
+  status?: "SUCCESS" | "ERROR";
+  message: string;
+  payload?: FormData;
+  fieldErrors: Record<string, string[] | undefined>;
+  timestamp: number;
+};
+
+export const EMPTY_ACTION_STATE: ActionState = {
+  message: "",
+  fieldErrors: {},
+  timestamp: Date.now(),
+};
 
 export const fromErrorToActionState = (
   error: unknown,
@@ -8,25 +20,46 @@ export const fromErrorToActionState = (
 ): ActionState => {
   if (error instanceof z.ZodError) {
     // if validation error with Zod, return first error message
-    const zodError = z.prettifyError(error);
+    // const zodError = z.prettifyError(error);
 
     return {
-      message: zodError,
+      status: "ERROR",
+      message: "",
       payload: formData,
+      fieldErrors: error.flatten().fieldErrors,
+      timestamp: Date.now(),
     };
   } else if (error instanceof Error) {
     // if another error instance, return error message
     // e.g. database error
     return {
+      status: "ERROR",
       message: error.message,
       payload: formData,
+      fieldErrors: {},
+      timestamp: Date.now(),
     };
   } else {
     // if not an error instance but something else crashed
     // return generic error message
     return {
+      status: "ERROR",
       message: "An unknown error occurred",
       payload: formData,
+      fieldErrors: {},
+      timestamp: Date.now(),
     };
   }
+};
+
+export const toActionState = (
+  status: ActionState["status"],
+  message: string,
+): ActionState => {
+  return {
+    status,
+    message,
+    fieldErrors: {},
+    timestamp: Date.now(),
+  };
 };
--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useActionState } from "react";
+import { FieldError } from "@/components/form/field-error";
 import { SubmitButton } from "@/components/form/submit-button";
+import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
@@ -15,7 +17,8 @@ type TicketUpsertFormProps = {
 const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
   const [actionState, action] = useActionState(
     upsertTicket.bind(null, ticket?.id),
-    { message: "" },
+    // Use the empty state as initial before the form ever got submitted
+    EMPTY_ACTION_STATE,
   );
 
   return (
@@ -29,6 +32,7 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
           (actionState.payload?.get("title") as string) ?? ticket?.title
         }
       />
+      <FieldError actionState={actionState} name="title" />
 
       <Label htmlFor="content">Content</Label>
       <Textarea
@@ -38,6 +42,7 @@ const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
           (actionState.payload?.get("content") as string) ?? ticket?.content
         }
       />
+      <FieldError actionState={actionState} name="content" />
 
       <SubmitButton label={ticket ? "Edit" : "Create"} />
 
